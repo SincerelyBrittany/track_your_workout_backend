abfdf28bbb2261b78b9ef4fa54a40c282df368db
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-25 21:24:31 -0400

    updates serializer

--- app/serializers/user_workouts_serializer.rb
+++ app/serializers/user_workouts_serializer.rb
@@ -2,7 +2,29 @@
 #   attributes :id
 # end
 
+# class UserWorkoutsSerializer
+#   include FastJsonapi::ObjectSerializer
+#   # attributes :id, :user_id, :workout_id, :date 
+#   # belongs_to :user
+#   # belongs_to :workout
+
+#   attributes  :date, :user_id, :user, :workout
+# end
+
 class UserWorkoutsSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes  :date, :user_id, :user, :workout
+  def initialize(workout_obj)
+    @user_workout = workout_obj
+  end 
+
+  def to_serialized_json
+    @user_workout.to_json(:except => [:created_at, :updated_at], 
+    :include => {
+        :workout => {:only => [:name, :url, :time]}, 
+        :user => {:only => [:name]} 
+    })
+  end 
+
+  def update_time
+    time.strftime("%I:%M:%S %p")
+  end
 end
--- app/serializers/workout_serializer.rb
+++ app/serializers/workout_serializer.rb
@@ -3,7 +3,14 @@
 # end
 
 
-class WorkoutSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :name, :url, :user_workouts
-end
+# class WorkoutSerializer
+#   include FastJsonapi::ObjectSerializer
+#   # attributes :name, :url, :time, :user_workouts
+#   has_many :user_workouts
+#   has_many :users
+
+#   def time
+#     time.strftime("%I:%M:%S %p")
+#   end
+
+# end

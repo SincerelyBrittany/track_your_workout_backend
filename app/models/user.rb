115266df920736c3ee6e26df4c4e876811adcaaa
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-02 08:48:20 -0400

    makes username presence true

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_many :user_workouts, dependent: :destroy
     has_many :workouts, through: :user_workouts
 
+    validates :username, presence: true
+
     def create_find_by_name(user_params)
         self.where(username: user_params).first_or_create do |user|
             user.username = user_params.username
